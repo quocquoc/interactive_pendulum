ad43bd41371902fa6739dee75e5cb8221ad98508
Quoc Truong <truongvanquoc@gmail.com>
2024-07-01 16:49:48 +0700

    Create React App for interactive Pendulum
    Create React App

---
+++ pendulum/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InteractivePendulum from './InteractivePendulum';
+
+function App() {
+  return (
+    <div className="App">
+      <InteractivePendulum />
+    </div>
+  );
+}
+
+export default App;
